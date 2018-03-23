f3425aff9ee81db2a260454b1560b1aefc120c9c
Will <wmccrory@gmail.com>
2018-03-22 20:29:55 -0500

    Adding note functionality

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,16 +1,46 @@
-console.log("Working");
-
 $(".addNote").on("click", function () {
 	$("#modal").animate({right: '-100%'})
 	console.log($(this).data("id"));
 	$("#modal").empty();
-	$("#modal").html("<h2>Add a note</h2><form><textarea id='submitText'></textarea><br><button data-id='" + $(this).data("id") + "'>Submit new note</button></form>")
+	$("#modal").html("<h2>Add a note</h2><textarea id='submitText'></textarea><br><button class='noteSubmit' data-id='" + $(this).data("id") + "'>Submit new note</button>")
 	setTimeout(function() {
 		$("#modal").animate({right: '1%'})
 	}, 100);
 })
 
 $(".viewNote").on("click", function () {
-	console.log($(this).data("id"));
-	$("#modal").animate({right: '1%'})
+	var id = $(this).data("id");
+
+	$.ajax({
+		method: "GET",
+		url: "/articles/" + id
+	}).then(function(data) {
+		console.log(data);
+		if (data.note) {
+			console.log(data.note.body);
+			$("#modal").empty();
+			$("#modal").html("<h2>Notes on this article: </h2><h3>" + data.note.body + "</h3>");
+			$("#modal").animate({right: '1%'})
+		}
+		else if (!data.note) {
+			$("#modal").empty();
+			$("#modal").html("<h2>No notes on this article");
+			$("#modal").animate({right: '1%'})
+		}
+	})
+
+	// $("#modal").animate({right: '1%'})
+})
+
+$("#modal").on("click", ".noteSubmit", function () {
+	var id = $(this).data("id");
+
+	$.ajax({
+		method: "POST",
+		url: "/articles/" + id,
+		data: {body: $("#submitText").val()}
+	}).then(function() {
+		$("#modal").empty();
+		$("#modal").html("<h2>Your note has been submitted");
+	})
 })
